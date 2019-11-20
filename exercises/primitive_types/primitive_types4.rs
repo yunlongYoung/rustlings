e2fb993bd709bf2b31929188655ba394f415be2e
yunlongYoung <dragonlip@qq.com>
2019-11-21 00:23:12 +0800

    struct

---
+++ exercises/primitive_types/primitive_types4.rs
@@ -0,0 +1,12 @@
+// primitive_types4.rs
+// Get a slice out of Array a where the ??? is so that the `if` statement
+// returns true. Execute `rustlings hint primitive_types4` for hints!!
+
+#[test]
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let nice_slice = &a[1..4];
+
+    assert_eq!([2, 3, 4], nice_slice)
+}
